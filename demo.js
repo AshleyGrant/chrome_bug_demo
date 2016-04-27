8d9f710a56b419320feb05b40128be35424a34d5
Ashley Grant <AshleyGrant@users.noreply.github.com>
2016-04-26 23:14:47 -0400

    creating a stack

--- demo.js
+++ demo.js
@@ -1,3 +1,17 @@
-var stringWithStack = '\n--THIS WILL NOT REPEAT--\n' + new Error().stack + '\n--THIS WILL REPEAT--\n';
-console.log(stringWithStack);
-throw new Error(stringWithStack);
+function createError() {
+  throw new Error("Inner Error");
+}
+
+function callErrorFunction() {
+  try {
+    createError();
+  }
+  catch(innerError) {
+    var stringWithStack = '\n--THIS WILL NOT REPEAT--\n' + new innerError.stack + '\n--THIS WILL REPEAT--\n';
+    console.log(stringWithStack);
+    throw new Error(stringWithStack);
+  }
+}
+
+callErrorFunction();
+
